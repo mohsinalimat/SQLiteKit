bd7b664e3fcc4b5071387f2510c750076b6f6aa4
alexiscn <xshf87@126.com>
2018-07-19 22:27:13 +0800

    add Extensions

---
+++ Frameworks/SQLiteKit/SQLiteValueExtendable.swift
@@ -0,0 +1,20 @@
+//
+//  SQLiteValueExtendable.swift
+//  SQLiteKit
+//
+//  Created by xushuifeng on 2018/7/19.
+//
+
+import Foundation
+
+public struct SQLiteExpression {
+    
+}
+
+extension String {
+    
+    public func like(_ pattern: String) {
+        
+    }
+    
+}
